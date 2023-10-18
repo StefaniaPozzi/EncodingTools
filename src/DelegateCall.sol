2945f9e03f8572b97c493fcc982563140d4d434a
StefaniaPozzi <stefania.pozzi.it@gmail.com>
2023-10-18 06:49:41 +0200

    delegatecall

--- src/DelegateCall.sol
+++ src/DelegateCall.sol
@@ -6,26 +6,49 @@ contract CalledContract {
     uint256 public varStoredAtAddress0Number;
     address public varStoredAtAddress1Address;
     uint256 public varStoredAtAddress2Value;
+    uint256 public differentTypeLayout0;
+
+    constructor(address _owner) {
+        varStoredAtAddress1Address = _owner;
+    }
+    /**
+     * Unmodifiable variables from the caller:
+     * 1. msg.sender
+     * 2. msg.value
+     * 3. address(this) / storage variables > check following function
+     *
+     * He cannot access the Caller (3) unmodifiable values.
+     */
 
     function setVariables(uint256 _number) public payable {
         varStoredAtAddress0Number = _number;
-        varStoredAtAddress1Address = msg.sender;
-        varStoredAtAddress2Value = msg.value;
+        varStoredAtAddress1Address = msg.sender; //Target's caller, not Caller's caller
+        varStoredAtAddress2Value = msg.value; //Target's value
+        differentTypeLayout0 = 1;
+        // varStoredAtAddress3Address = address(this);
+        // this will be 0 -> it needs to be set after deployment !
     }
 }
 
 contract CallerContract {
+    //same storage layout of the Called contract
     uint256 public number;
     address public sender;
     uint256 public value;
+    bool public differentTypeLayout0;
 
-    function setVariablesDelegatecall(address _contract, uint256 _number) public payable {
+    /**
+     * The compiler sobstitutes setVariables with the delegatecall
+     * and executes the function as it were in this contract,
+     * except for the (3) unmodifiable fields
+     */
+    function setVariablesDelegatecall(address _contract, uint256 _number) public payable returns (bool) {
         (bool success, bytes memory data) =
             _contract.delegatecall(abi.encodeWithSignature("setVariables(uint256)", _number));
+        return success;
     }
 
-     function setVariablesCall(address _contract, uint256 _number) public payable {
-        (bool success, bytes memory data) =
-            _contract.call(abi.encodeWithSignature("setVariables(uint256)", _number));
+    function setVariablesCall(address _contract, uint256 _number) public payable {
+        (bool success, bytes memory data) = _contract.call(abi.encodeWithSignature("setVariables(uint256)", _number));
     }
 }
--- test/DelegateCallTest.t.sol
+++ test/DelegateCallTest.t.sol
@@ -8,26 +8,15 @@ import {CalledContract, CallerContract} from "../src/DelegateCall.sol";
 contract DelegateCallTest is Test {
     CalledContract public bob;
     CallerContract public alice;
+    address someOwner = makeAddr("someOwner");
 
     function setUp() external {
-        bob = new CalledContract();
+        bob = new CalledContract(someOwner);
         alice = new CallerContract();
-    }
-
-    function test_Delegatecall() public {
         alice.setVariablesDelegatecall(address(bob), 123);
-        //Bob is not modified :(
-        assert(bob.varStoredAtAddress0Number() == 0);
-        assert(bob.varStoredAtAddress1Address() == address(0));
-        assert(bob.varStoredAtAddress2Value() == 0);
-        //Alice is modified!! :)
-        assert(alice.number() == 123);
-        assert(alice.sender() == address(this)); //Test contract is calling Alice's function
-            // assert(alice.value() == 456) ; value?
     }
 
-    function test_Call() public {
-        alice.setVariablesCall(address(bob), 123);
+    function test_call() public {
         //Bob is modified!! :)
         assert(bob.varStoredAtAddress0Number() == 123);
         assert(bob.varStoredAtAddress1Address() == address(alice)); //Alice is calling Bob's function
@@ -35,5 +24,26 @@ contract DelegateCallTest is Test {
         //Alice is not modified :(
         assert(alice.number() == 0);
         assert(alice.sender() == address(0));
+    }
+
+    function test_delegatecall() public {
+        //Bob is not modified :(
+        assert(bob.varStoredAtAddress0Number() == 0);
+        assert(bob.varStoredAtAddress1Address() == address(0));
+        assert(bob.varStoredAtAddress2Value() == 0);
+        //Alice state variables are modified!! :)
+        assert(alice.number() == 123);
+        assert(alice.sender() == address(this)); //Test contract is calling Alice's function
+            // assert(alice.value() == 456) ; value?
+    }
+
+    function test_addressThisDoesNotChangeItsValue() public {}
+
+    function test_differentLayoutStorage() public {
+        assert(alice.differentTypeLayout0()); //this could generate unexpectable behaviour
+    }
+
+    function test_delegatecallOnEOA() public {
+        assert(alice.setVariablesDelegatecall(someOwner, 123)); //calling delegate cal on EOA return success
     }
 }
